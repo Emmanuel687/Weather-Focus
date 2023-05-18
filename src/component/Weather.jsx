b390d5f82dca5faa1d6d4911451a77a8d6d0a377
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-05-18 15:06:08 +0300

    Dev:-Added Weather component

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -6,9 +6,15 @@ const Weather = () => {
     <div className='weather'>
         <h1>New York</h1>
         <ul>
-            <li>Wednesday</li>
+            <li>Wednesday 07:00</li>
             <li>Most Cloudy</li>
         </ul>
+
+        <div className="row">
+            <div className="col-6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" />
+            </div>
+        </div>
     </div>
   )
 }
