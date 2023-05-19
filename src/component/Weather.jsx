4fc266177c261ffc1e354c7ec41b52a2f6912f47
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-05-19 13:02:04 +0300

    Dev:-Added Submitted

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import { useState, useEffect } from "react";
 
 const Weather = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState("");
-  const[loading,setLoading] = useState(false)
+  const [submitted, setSubmitted] = useState(false); // Track form submission
 
   //API Credentials
   const apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
@@ -18,7 +17,7 @@ const Weather = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true)
+    setSubmitted(true); // Update form submission status
 
     axios
       .get(apiUrl)
@@ -40,55 +39,59 @@ const Weather = () => {
       });
   };
 
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-9">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Input City"
+            autoFocus="on"
+            onChange={handleCity}
+          />
+        </div>
+        <div className="col-3">
+          <input className="btn btn-primary w-100" type="submit" />
+        </div>
+      </div>
+    </form>
+  );
+
   return (
     <div className="weather">
-        
-      {/* Form */}
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Input City"
-              autoFocus="on"
-              onChange={handleCity}
-            />
-          </div>
-          <div className="col-3">
-            <input className="btn btn-primary w-100" type="submit" />
-          </div>
-        </div>
-      </form>
-      {/* Form */}
-      
-
-      <div>
-        <h1>{weather.city}</h1>
-        <ul>
-          <li>Wednesday 07:00</li>
-          <li>{weather.description}</li>
-        </ul>
-      </div>
-
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src={`https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`}
-            alt="mostly cloudy"
-          />
-          <span className="temperature">{weather.temperature}</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
+      {submitted ? (
+        <div>
+          {form}
+          <h1>{weather.city}</h1>
           <ul>
-            <li>Precipitation:- {weather.precipitation}%</li>
-            <li>Humidity:- {weather.humidity}%</li>
-            <li>Wind:-{weather.wind}Km/hr </li>
+            <li>Wednesday 07:00</li>
+            <li>{weather.description}</li>
           </ul>
+
+          <div className="row mt-3">
+            <div className="col-6">
+              <img
+                src={`https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`}
+                alt="mostly cloudy"
+              />
+              <span className="temperature">{weather.temperature}</span>
+              <span className="unit">°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Precipitation:- {weather.precipitation}%</li>
+                <li>Humidity:- {weather.humidity}%</li>
+                <li>Wind:-{weather.wind}Km/hr </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-      
+      ) : (
+        <div>
+          {form}
+        </div>
+      )}
     </div>
   );
 };
