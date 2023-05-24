ec46022f1cce30b0d95b0606954b2d4c4d0c202b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-05-24 17:48:39 +0300

    Dev:-Commented  WeatherDate Component

--- src/component/WeatherDate.jsx
+++ src/component/WeatherDate.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+// import React from "react";
 
-function WeatherDate({ currentDate }) {
-//  const days = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"]
+// function WeatherDate({ currentDate }) {
+// //  const days = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"]
 
-//   const day = days[currentDate.getDay()]
-//   const hours = currentDate.getHours();
-//   const minutes = currentDate.getMinutes();
-//   const month = currentDate.getMonth();
-//   if(hours<10){
-//     hours=`0${hours}`
-//   }
+// //   const day = days[currentDate.getDay()]
+// //   const hours = currentDate.getHours();
+// //   const minutes = currentDate.getMinutes();
+// //   const month = currentDate.getMonth();
+// //   if(hours<10){
+// //     hours=`0${hours}`
+// //   }
 
-//   if(minutes<0){
-//     minutes=`0${minutes}`
-//   }
+// //   if(minutes<0){
+// //     minutes=`0${minutes}`
+// //   }
 
-  return (
-    <div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
 
-export default WeatherDate;
+// export default WeatherDate;
