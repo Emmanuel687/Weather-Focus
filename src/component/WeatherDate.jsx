2e1179a38a03391c17d50cb8b5ef6f0624e4b2c7
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-05-25 12:54:58 +0300

    Dev:-Added Day

--- src/component/WeatherDate.jsx
+++ src/component/WeatherDate.jsx
@@ -15,7 +15,11 @@ function WeatherDate({currentDate}) {
     let dayWeek = daysWeek.at(day)
 
     if(minutes<10){
-        minutes= "0"+ minutes
+        minutes= `0${minutes}`
+    }
+    if(hour<10){
+        hour=`0${hour}`
+
     }
     
 
