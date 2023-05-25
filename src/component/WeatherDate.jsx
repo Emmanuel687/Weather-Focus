e3c128f1a5b7e01cfa11d257721b0b9d494e61b1
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-05-25 12:50:06 +0300

    Dev:-Added Day

--- src/component/WeatherDate.jsx
+++ src/component/WeatherDate.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 function WeatherDate({currentDate}) {
     let date = new Date(currentDate*1000);
+    
+    let daysWeek = [
+        "Sunday", "Monday", "Tuesday" , "Wenesday", "Thursday", "Friday","Saturday"
+    ]
 
     let day = date.getDay();
     let month = date.getMonth();
     let year = date.getFullYear();
     let hour = date.getHours()
     let minutes = date.getMinutes();
+    let dayWeek = daysWeek.at(day)
+
+    if(minutes<10){
+        minutes= "0"+ minutes
+    }
+    
 
     console.log(`Day is ${date}`)
     console.log(`Hour is ${hour}`)
@@ -16,7 +26,7 @@ function WeatherDate({currentDate}) {
     console.log(`Month is ${month}`)
     console.log(`Year is ${year}`)
   return (
-    <div>{hour}:{minutes}</div>
+    <div>{dayWeek}  {hour}:{minutes}</div>
   )
 }
 
