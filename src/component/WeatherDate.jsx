9e70c0e64b06a63d4bd513f11f588e388a51de0a
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-05-26 11:46:02 +0300

    Dev:-Set Days of Weeks to UTC Format

--- src/component/WeatherDate.jsx
+++ src/component/WeatherDate.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 // Received Props from Weather Component
 function WeatherDate({currentDate}) {
+    // Change Date to UTC format
     let date = new Date(currentDate*1000);
+
+    // Set Days of Week
     let daysWeek = [
         "Sunday", "Monday", "Tuesday" , "Wednesday", "Thursday", "Friday","Saturday"
     ]
-
+    
+    
     let day = date.getDay();
-    let month = date.getMonth();
-    let year = date.getFullYear();
     let hour = date.getHours()
     let minutes = date.getMinutes();
     let dayWeek = daysWeek.at(day)
